613a04973c3e53671d47d49d4899c90dbf482ec8
Elam Nabi <elam@nabi.nu>
2020-09-16 19:24:12 +0200

    Frontend and backend map-structure done

---
+++ api/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Welcome to the API.'
+    })
+})
+
+app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
