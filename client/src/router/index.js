289e971072952810a1739b42f275b1d5b489a26b
Elam Nabi <elam@nabi.nu>
2020-09-17 19:00:18 +0200

    test RED on the meetups test-file

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -11,13 +10,10 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: "/meetups",
+    name: "meetups",
+    component: () => import("@/views/Meetups.vue"),
+  },
 ]
 
 const router = new VueRouter({
