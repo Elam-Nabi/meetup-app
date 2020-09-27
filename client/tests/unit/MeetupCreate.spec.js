bfe35a5206431232011e41bf579503b8ed03f831
Elam Nabi <elam@nabi.nu>
2020-09-27 21:30:16 +0200

    meetup create tests done

--- client/tests/unit/MeetupCreate.spec.js
+++ client/tests/unit/MeetupCreate.spec.js
@@ -1,14 +1,38 @@
-import { mount } from '@vue/test-utils'
+import Vuex from 'vuex'
+import { mount, createLocalVue } from '@vue/test-utils'
 import MeetupCreate from '@/views/MeetupCreate.vue'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+localVue.filter('moment', () => 'moment')
+
+const store = new Vuex.Store({
+    state: {
+        reviewEvents: []
+    }
+})
 
 describe('MeetupCreate', () => {
 
     let wrapper;
     beforeEach(() => {
-        wrapper = mount(MeetupCreate);
+        wrapper = mount(MeetupCreate, {
+            store,
+            localVue
+        });
     });
 
     test('Should check if component exist', () => {
         expect(wrapper.findComponent(MeetupCreate).exists()).toBe(true)
+    })
+
+    test('should check if props is set in component', async () => {
+        await wrapper.setProps({
+            event: {
+                title: 'My name is Elam'
+            }
+        })
+        expect(wrapper.vm.event.title).toBe('My name is Elam')
     })
 }); 
