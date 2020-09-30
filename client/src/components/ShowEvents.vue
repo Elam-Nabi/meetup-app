7dcd260e610ca1f85905a60bbfe64cbfd24fd7bc
Elam Nabi <elam@nabi.nu>
2020-09-30 11:21:25 +0200

    Fixed some stuff, before deployment

--- client/src/components/ShowEvents.vue
+++ client/src/components/ShowEvents.vue
@@ -35,7 +35,7 @@ export default {
   },
   async created() {
     try {
-      const res = await axios.get("http://localhost:5000/meetups");
+      const res = await axios.get("/meetups");
       this.events = res.data;
     } catch (error) {
       this.error = "Something went wrong!";
